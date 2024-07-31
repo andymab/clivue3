17083ba8b29f3b6ce54f4816afb635a2d4f0fcdd
home1 Andrey Minaev <650517@355042.ru>
2024-07-31 07:31:35 +0300

    bar

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <v-app-bar density="compact" elevation="0" color="primary">
+      <v-breadcrumbs class="" :items="breadcrumbs" divider="-">
+        <template #item="{ item }">
+          <a href="#" @click="handleBreadcrumbsClick(item)">{{ item.title }}</a>
+        </template>
+      </v-breadcrumbs>
       <v-btn icon>
         <v-icon>mdi-file-edit</v-icon>
       </v-btn>
@@ -11,7 +16,7 @@
       <v-btn icon>
         <v-icon>mdi-close-thick</v-icon>
       </v-btn>
-      
+
     </v-app-bar>
     <v-card :loading="loading" flat>
       <div class="d-flex justify-space-between align-center item-block py-2">
@@ -28,11 +33,7 @@
                 {{ description }}
               </v-card-subtitle>
             </v-card-item>
-            <v-breadcrumbs class="py-0 px-0 custom-breadcrumbs" color="primary" :items="breadcrumbs" divider="-">
-              <template #item="{ item }">
-                <a href="#" @click="handleBreadcrumbsClick(item)">{{ item.title }}</a>
-              </template>
-            </v-breadcrumbs>
+
           </div>
         </div>
         <div class="d-flex flex-column">
@@ -75,6 +76,10 @@ export default {
 
   },
   mounted() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  },
+  beforeUnmount() {
+    // window.removeEventListener('keydown', this.handleKeyDown);
   },
   methods: {
     getlocaleDate(timestamp) {
@@ -97,6 +102,7 @@ export default {
         this.time_create = this.getlocaleDate(this.serverData.time_create);
         this.restime = this.getlocaleDate(this.serverData.restime);
         this.breadcrumbs = this.serverData.breadcrumbs;
+        this.breadcrumbs.splice(0, 0, { title: 'Home', href: '/articles' });
 
         this.convertedMarkdown = marked(this.serverData.post); //'# Marked in Node.js\n\nRendered by **marked**.'  this.serverData.
         //Использование  this.$nextTick()  гарантирует, что код подсветки синтаксиса будет вызван после обновления DOM
@@ -112,6 +118,29 @@ export default {
       } catch (error) {
         console.error(error);
         this.loading = false
+      }
+    },
+    async handleBreadcrumbsClick(id) {
+      // this.loading = true
+      // const response = await api.getArticles(id);
+      // this.serverItems = response.data;
+      // this.totalItems = this.serverItems.length;
+      // this.loading = false
+    },
+
+    handleKeyDown(event) {
+      if (event.ctrlKey && (event.key === 's' || event.key === 'ы')  ) {
+        console.log('save');
+        event.preventDefault();
+      }
+      if (event.ctrlKey && (event.key === 'e'  || event.key === 'у')) {
+        console.log('edit');
+        event.preventDefault();
+
+      }
+      if (event.ctrlKey && (event.key === 'x'  || event.key === 'ч')) {
+        console.log('delete');
+        event.preventDefault();
       }
     },
   },
