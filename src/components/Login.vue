78ac8465527d9afc23d2444d5f702cd972657443
Andy hu <650517@355042.ru>
2024-08-02 19:56:13 +0300

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,29 @@
 <template>
-    <v-sheet class="mx-auto" max-width="300">
-        <v-card>
-            <v-form validate-on="submit lazy" @submit.prevent="login">
-                <v-text-field v-model="email" :rules="emailRules" type="email" label="Email" required></v-text-field>
-                <v-text-field v-model="password" type="password" label="Пароль" required></v-text-field>
-                <v-btn :loading="loading" class="mt-2" text="Отправить" type="submit" block></v-btn>
-            </v-form>
-        </v-card>
-    </v-sheet>
+    <!-- <v-sheet class="mx-auto"   persistent  full-width  width="auto"> -->
+    
+    <v-dialog v-model="loginDialog" persistent  full-width  width="auto">
+              <v-card  max-width="400">
+                <v-card-title>Пройдите авторизацию</v-card-title>
+                <v-card-text>            
+                    <v-form validate-on="submit lazy" @submit.prevent="login">
+                    <v-text-field color="primary" variant="underlined" v-model="email" :rules="emailRules" type="email" label="Email" required></v-text-field>
+                    <v-text-field color="primary" variant="underlined" v-model="password" type="password" label="Пароль" required></v-text-field>
+                    <v-btn :loading="loading" class="mt-2" text="Отправить" type="submit" block></v-btn>
+                </v-form>
+                </v-card-text>
+                </v-card>
+    </v-dialog>
+    <!-- </v-sheet> -->
 </template>
 
 <script>
 import api from "../api"
 
 export default {
+    name:'Login',
     data() {
         return {
+            loginDialog:true,
             emailRules: [
                 value => {
                     var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
